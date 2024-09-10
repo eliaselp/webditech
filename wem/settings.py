dd8e2151e69315870ad32b94096875bce84676e8
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-09-10 00:23:14 -0700

    Update settings.py

--- wem/settings.py
+++ wem/settings.py
@@ -116,12 +116,10 @@ STATIC_ROOT = BASE_DIR / 'colectstatics'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-SECURE_SSL_REDIRECT = True
+
 
 
 CSRF_COOKIE_SECURE = True # O True si usas HTTPS
 CSRF_TRUSTED_ORIGINS = ['https://wemautomations.ddns.net','http://localhost:8000']
 
 
-CSRF_COOKIE_SECURE = True # O True si usas HTTPS
-CSRF_TRUSTED_ORIGINS = ['https://wemautomations.ddns.net','http://localhost:8000','http://localhost']
