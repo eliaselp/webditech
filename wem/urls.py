27db57b34fb3028f548cf1a1d6ef2a8a5efe8484
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-09-10 01:16:35 -0400

    Add files via upload

---
+++ wem/urls.py
@@ -0,0 +1,19 @@
+#from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('',include('App.urls')),
+]
+
+#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+'''
+from django.conf.urls import handler404
+from cliente.views import custom_404
+
+handler404 = custom_404
+'''
