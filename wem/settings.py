ec7f464886c3ffa6ad2b08f4704164e6c7ce820f
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-09-10 00:34:11 -0700

    Update settings.py

--- wem/settings.py
+++ wem/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
-STATIC_ROOT = BASE_DIR / 'wemautomations'
+STATIC_ROOT = BASE_DIR / 'collectstatics'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
